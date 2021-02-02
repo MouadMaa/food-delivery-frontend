4741336673117b4816498d8a11d127366fae1916
mouadmaa <maaroufimouad00@gmail.com>
2021-02-02 20:51:48 +0100

    filter restaurant with selected category

--- src/components/home/restaurants/restaurants.component.tsx
+++ src/components/home/restaurants/restaurants.component.tsx
@@ -2,12 +2,21 @@ import { FC } from 'react'
 import { useSelector } from 'react-redux'
 
 import { restaurantsSelector } from '@/store/restaurant/restaurant.selectors'
+import { selectedCategorySelector } from '@/store/category/category.selectors'
 import RestaurantCard from '../restaurant-card/restaurant-card.component'
 import SortBy from '../sort-by/sort-by.component'
 import { StyledRestaurants } from './restaurants.styles'
 
 const Restaurants: FC = () => {
 	const restaurants = useSelector(restaurantsSelector)
+	const selectedCategory = useSelector(selectedCategorySelector)
+
+	let filteredRestaurantsBySelectedCategory = restaurants
+	if (selectedCategory.id !== 'all') {
+		filteredRestaurantsBySelectedCategory = restaurants.filter((restaurant) =>
+			restaurant.categories.find((category) => category.id === selectedCategory.id),
+		)
+	}
 
 	return (
 		<StyledRestaurants>
@@ -15,7 +24,11 @@ const Restaurants: FC = () => {
 				<h2>Popular Near You</h2>
 				<SortBy />
 			</article>
-			<div>{restaurants.map((restaurant) => <RestaurantCard key={restaurant.id} restaurant={restaurant} />)}</div>
+			<div>
+				{filteredRestaurantsBySelectedCategory.map((restaurant) => (
+					<RestaurantCard key={restaurant.id} restaurant={restaurant} />
+				))}
+			</div>
 		</StyledRestaurants>
 	)
 }

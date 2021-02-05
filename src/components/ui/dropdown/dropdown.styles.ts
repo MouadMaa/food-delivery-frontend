94fbce637103c0bf0404afff9c6c357b9a3d351b
mouadmaa <maaroufimouad00@gmail.com>
2021-02-06 00:48:12 +0100

    implement filter restaurants on serach category selected

--- src/components/ui/dropdown/dropdown.styles.ts
+++ src/components/ui/dropdown/dropdown.styles.ts
@@ -6,7 +6,7 @@ export const StyledDropdown = styled.div(() => [
   `,
 	css`
 		button {
-			${tw`flex items-center justify-between w-full px-4 py-2 space-x-2 text-sm text-gray-600 tracking-wide transition-colors duration-300 hover:(bg-gray-100 text-gray-700) focus:outline-none dark:(text-gray-100 hover:bg-gray-700)`};
+			${tw`flex items-center justify-between w-full px-4 py-2 space-x-2 text-sm text-gray-600 tracking-wide transition-colors duration-300 hover:(bg-gray-200 text-gray-700) focus:outline-none dark:(text-gray-100 hover:bg-gray-700)`};
 
 			svg {
 				${tw`w-6 h-6 stroke-current`};
--- src/layout/header/search-dropdown/search-dropdown.styles.ts
+++ src/layout/header/search-dropdown/search-dropdown.styles.ts
@@ -4,10 +4,10 @@ export const StyledSearchDropdown = styled.div(() => [
 	tw`absolute left-0 top-12 w-80 p-4 space-y-3 bg-gray-50 rounded-xl overflow-hidden shadow-lg transform -translate-x-5 dark:bg-gray-800`,
 	css`
 		article span {
-			${tw`text-primary-dark`};
+			${tw`text-primary-dark dark:text-primary-default`};
 		}
 		article div {
-			${tw`text-gray-600 py-1 px-3 cursor-pointer rounded-lg transition-colors duration-300 hover:text-gray-700 hover:bg-gray-100`};
+			${tw`tracking-wider text-gray-600 py-1 px-3 cursor-pointer rounded-lg transition-colors duration-300 hover:text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700`};
 		}
 	`,
 ])

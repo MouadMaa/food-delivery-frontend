629e6119025d234a03f5e996f8c97d903c6636fb
mouadmaa <maaroufimouad00@gmail.com>
2021-03-14 12:27:28 +0100

    style scroll bar

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -18,6 +18,32 @@ const GlobalStyles = createGlobalStyle`
     margin: 0 auto;
   }
 
+  /* Scroll Bar */
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: #F5F5F5 #ED5359;
+  }
+  ::-webkit-scrollbar-track
+  {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar
+  {
+    width: 7px;
+    height: 7px;
+    background-color: #F5F5F5;
+  }
+  ::-webkit-scrollbar-thumb
+  {
+    border-radius: 10px;
+    background-color: #ED5359;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #DF494E;
+  }
+
   /* NProgress */
   #nprogress > div.bar {
     background-color: #ED5359;
